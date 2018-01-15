7c8389be351535583857fb749c54e53058070150
Anton Kostryukov <anton97kos@gmail.com>
2018-01-15 16:04:16 +0300

    update

--- js/MenuSearch.js
+++ js/MenuSearch.js
@@ -1,201 +1,191 @@
 class Post {
-  constructor(title) {
-    this.title = title;
-  }
+    constructor(title) {
+        this.title = title;
+    }
 }
 
-class Quotation{
-  constructor(time, rating, rev24, nethash, coast, capitalization, size) {
-    this.time = time;
-    this.rating = rating;
-    this.rev24 = rev24;
-    this.nethash = nethash;
-    this.coast = coast;
-    this.capitalization = capitalization;
-    this.size = size;
-  }
+class Quotation {
+    constructor(time, rating, rev24up, rew24down, nethash, capitalization_up, capitalization_down) {
+        this.time = time;
+        this.rating = rating;
+        this.rev24up = rev24up;
+        this.rev24down = rev24down;
+        this.nethash = nethash;
+        this.capitalization_up = capitalization_up;
+        this.capitalization_down = capitalization_down;
+    }
 }
 
-new Vue ({
-  el: '#app-list',
-  data: {
-      dataList: [],
-    search: '',
-    postList : [
-      new Post(
-        'Altcommunity(ALTCOM)'
-      ),
-      new Post(
-        'BitcoinGold(BTG)'
-      ),
-      new Post(
-        'Bytecoin(BCN)'
-      ),
-      new Post(
-        'Decred(DCR)'
-      ),
-      new Post(
-        'DGB-Groestl(DGB)'
-      ),
-      new Post(
-        'DigitalNote(XDN)'
-      ),
-      new Post(
-        'Electroneum(ETN)'
-      ),
-      new Post(
-        'Ellaism(ELLA)'
-      ),
-      new Post(
-        'Ethereum(ETH)'
-      ),
-      new Post(
-        'EthereumClassic(ETC)'
-      ),
-      new Post(
-        'Expanse(EXP)'
-      ),
-      new Post(
-        'Feathercoin(FTC)'
-      ),
-      new Post(
-        'GoByte(GBX)'
-      ),
-      new Post(
-        'GroestlCoin(GRS)'
-      ),
-      new Post(
-        'Halcyon(HAL)'
-      ),
-      new Post(
-        'Hush(HUSH)'
-      ),
-      new Post(
-        'Karbo(KRB)'
-      ),
-      new Post(
-        'Komodo(KMD)'
-      ),
-      new Post(
-        'LBRY(LBC)'
-      ),
-      new Post(
-        'Metaverse(ETP)'
-      ),
-      new Post(
-        'Monacoin(MONA)'
-      ),
-      new Post(
-        'Monero(XMR)'
-      ),
-      new Post(
-        'Musicoin(MUSIC)'
-      ),
-      new Post(
-        'Myriad-Groestl(XMY)'
-      ),
-      new Post(
-        'Orbitcoin(ORB)'
-      ),
-      new Post(
-        'Pascalcoin(PASC)'
-      ),
-      new Post(
-        'PascalLite(PASL)'
-      ),
-      new Post(
-        'Pirl(PIRL)'
-      ),
-      new Post(
-        'Sia(SC)'
-      ),
-      new Post(
-        'Sibcoin(SIB)'
-      ),
-      new Post(
-        'Sumokoin(SUMO)'
-      ),
-      new Post(
-        'Trezarcoin(TZC)'
-      ),
-      new Post(
-        'Verge-Groestl(XVG)'
-      ),
-      new Post(
-        'Verge-Lyra2REv2(XVG)'
-      ),
-      new Post(
-        'Vertcoin(VTC)'
-      ),
-      new Post(
-        'Vivo(VIVO)'
-      ),
-      new Post(
-        'Ubiq(UBQ)'
-      ),
-      new Post(
-        'Zcash(ZEC)'
-      ),
-      new Post(
-        'Zclassic(ZCL)'
-      ),
-      new Post(
-        'Zencash(ZEN)'
-      )
-]
-  },
+new Vue({
+    el: '#app-list',
+    data: {
+        dataList: [],
+        search: '',
+        postList: [
+            new Post(
+                'Altcommunity(ALTCOM)'
+            ),
+            new Post(
+                'BitcoinGold(BTG)'
+            ),
+            new Post(
+                'Bytecoin(BCN)'
+            ),
+            new Post(
+                'Decred(DCR)'
+            ),
+            new Post(
+                'DGB-Groestl(DGB)'
+            ),
+            new Post(
+                'DigitalNote(XDN)'
+            ),
+            new Post(
+                'Electroneum(ETN)'
+            ),
+            new Post(
+                'Ellaism(ELLA)'
+            ),
+            new Post(
+                'Ethereum(ETH)'
+            ),
+            new Post(
+                'EthereumClassic(ETC)'
+            ),
+            new Post(
+                'Expanse(EXP)'
+            ),
+            new Post(
+                'Feathercoin(FTC)'
+            ),
+            new Post(
+                'GoByte(GBX)'
+            ),
+            new Post(
+                'GroestlCoin(GRS)'
+            ),
+            new Post(
+                'Halcyon(HAL)'
+            ),
+            new Post(
+                'Hush(HUSH)'
+            ),
+            new Post(
+                'Karbo(KRB)'
+            ),
+            new Post(
+                'Komodo(KMD)'
+            ),
+            new Post(
+                'LBRY(LBC)'
+            ),
+            new Post(
+                'Metaverse(ETP)'
+            ),
+            new Post(
+                'Monacoin(MONA)'
+            ),
+            new Post(
+                'Monero(XMR)'
+            ),
+            new Post(
+                'Musicoin(MUSIC)'
+            ),
+            new Post(
+                'Myriad-Groestl(XMY)'
+            ),
+            new Post(
+                'Orbitcoin(ORB)'
+            ),
+            new Post(
+                'Pascalcoin(PASC)'
+            ),
+            new Post(
+                'PascalLite(PASL)'
+            ),
+            new Post(
+                'Pirl(PIRL)'
+            ),
+            new Post(
+                'Sia(SC)'
+            ),
+            new Post(
+                'Sibcoin(SIB)'
+            ),
+            new Post(
+                'Sumokoin(SUMO)'
+            ),
+            new Post(
+                'Trezarcoin(TZC)'
+            ),
+            new Post(
+                'Verge-Groestl(XVG)'
+            ),
+            new Post(
+                'Verge-Lyra2REv2(XVG)'
+            ),
+            new Post(
+                'Vertcoin(VTC)'
+            ),
+            new Post(
+                'Vivo(VIVO)'
+            ),
+            new Post(
+                'Ubiq(UBQ)'
+            ),
+            new Post(
+                'Zcash(ZEC)'
+            ),
+            new Post(
+                'Zclassic(ZCL)'
+            ),
+            new Post(
+                'Zencash(ZEN)'
+            )
+        ]
+    },
     created() {
-        fetch('https://jsonplaceholder.typicode.com/posts/1/')
+        fetch('http://jutaxe.com/api.php')
             .then((response) => {
-                if(response.ok) {
+                if (response.ok) {
                     return response.json();
                 }
                 throw new Error('Network response was not ok');
             })
             .then((json) => {
-
-                /*
-                создать массив x in array, где массив - JSON из подJSON, х - key {
-                    внутри написать логику с добавлением, т.е. то, что ниже написано
-                    скорее всего, vue продублирует
+                for (const obj of json) {
+                    this.dataList.push({
+                        time: obj.queue,
+                        rating: obj.exrate,
+                        rev24up: obj.rev24up,
+                        rev24down: obj.rev24down,
+                        nethash: obj.nethash,
+                        capitalization_up: obj.market_cap_up,
+                        capitalization_down: obj.market_cap_down
+                    });
                 }
-                */
+    })
+    .
+        catch((error) => {
+            console.log(error);
 
-                this.dataList.push({
-                        title: json.title, //title и body - поля JSON
-                        body: json.body
-                        /*
-                        time = json.time,
-                        rating = json.rating,
-                        rev24 = json.rev24,
-                        nethash = json.nethash,
-                        coast = json.coast,
-                        capitalization = json.capitalization,
-                        size = json.size
-                        */
-                    }
-                );
-            })
-            .catch((error) => {
-                console.log(error);
-
-            });
+        });
     },
-  methods: {
-    openData: function (event) {
-      alert('Hello');
-      if (event) {
-        alert(event.target.innerHTML);
-      }
+    methods: {
+        openData: function (event) {
+            alert('Hello');
+            if (event) {
+                alert(event.target.innerHTML);
+            }
+        }
+    },
+    computed: {
+        filteredList() {
+            return this.postList.filter(post => {
+                return post.title.toLowerCase().includes(this.search.toLowerCase())
+            })
+        }
     }
-  },
-  computed: {
-    filteredList() {
-      return this.postList.filter(post => {
-        return post.title.toLowerCase().includes(this.search.toLowerCase())
-      })
-    }
-  }
 });
 
 
